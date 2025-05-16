0b517ec4657c61ba35dfb32b3ef662f684d4fc8d
papen12 <papenfabri2.0@gmail.com>
2025-05-16 19:52:12 -0400

    dependencias

--- backend/Routes/auth.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { AuthController } from '../Controllers/AuthController';
-
-const router = express.Router();
-router.post('/login', AuthController.login);
-export default router;
--- backend/src/Config/supabase.ts
+++ backend/src/Config/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import { env } from 'process';
 
-export const supabase = createClient(
-    process.env.SUPABASE_URL || '',
-    process.env.SUPABASE_ANON_KEY || ''
-);
+const supabaseUrl = env.SUPABASE_URL || 'https://agdulojbkcfmbuqtfwb.supabase.co';
+const supabaseKey = env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdXBhYmFzZS1hZG1pbiIsImlhdCI6MTY4NjQ2NzY0MCwiZXhwIjoxOTgyMDI3NjQwfQ.7gk8v0xqZbKXy2j4m5n7f3g5f3g5f3g5f3g5f3g5f3g';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+

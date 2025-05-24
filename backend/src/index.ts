a6cdab57a654ea6b997ae182f0856be71d014004
papen12 <papenfabri2.0@gmail.com>
2025-05-24 12:15:39 -0400

    correccion main

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import personaRouter from '../Routes/PersonaRoutes'; 
-<<<<<<< HEAD
-import clienteRouter from '../Routes/ClienteRoutes';
-=======
 import autorRouter from '../Routes/AutorRouter';
->>>>>>> 4294fc611e6f9b861720acda7e92663872ba1ce4
 dotenv.config();
 
 
@@ -21,12 +17,8 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/persona', personaRouter);
-<<<<<<< HEAD
-app.use('/api/cliente',clienteRouter)
-=======
 app.use('/api/autor', autorRouter)
 
->>>>>>> 4294fc611e6f9b861720acda7e92663872ba1ce4
 
 app.get('/api', (_req, res) => {
   res.json({ message: "Hola desde Express!" });

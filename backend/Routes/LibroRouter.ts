e01e5412bd3c861d5265808d9b4e214ddf468d64
papen12 <papenfabri2.0@gmail.com>
2025-06-10 17:42:53 -0400

    backend actualizado

--- backend/Routes/LibroRouter.ts
+++ backend/Routes/LibroRouter.ts
@@ -5,9 +5,9 @@ import { Router } from "express";
 const libroRouter=Router()
 
 libroRouter.get("/", LibroController.getLibro);
-libroRouter.get("/libro/buscar", LibroController.getLibroTitulo);
 libroRouter.get("/id/:id", LibroController.getLibroId);
-libroRouter.get("/titulo/:titulo", LibroController.getLibroTitulo);
+libroRouter.get("/titulo/:titulo", LibroController.getLibroPorTitulo);
+libroRouter.get("/genero/:genero",LibroController.getLibroPorGenero)
 
 
 
